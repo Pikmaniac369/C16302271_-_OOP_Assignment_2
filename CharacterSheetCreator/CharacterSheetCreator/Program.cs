9323585c99a4b150348a59e76948f183d3f8d672
Adam Ryan <C16302271@mydit.ie>
2018-04-20 22:16:18 +0100

    Created the actual C# file

---
+++ CharacterSheetCreator/CharacterSheetCreator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterSheetCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stops the command window from closing immediately
+            Console.ReadKey();
+        }
+    }
+}
