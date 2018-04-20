cb0ee315ca75d79e70f50023c2160a22e33293bc
Adam Ryan <C16302271@mydit.ie>
2018-04-20 23:19:24 +0100

    Created the basic outline for my menu method

--- CharacterSheetCreator/CharacterSheetCreator/Program.cs
+++ CharacterSheetCreator/CharacterSheetCreator/Program.cs
@@ -11,8 +11,16 @@ namespace CharacterSheetCreator
     {
         static void Main(string[] args)
         {
+            //Display the menu:
+            displayMenu();
+
             //Stops the command window from closing immediately
             Console.ReadKey();
+        }
+
+        private static void displayMenu()
+        {
+            Console.WriteLine("Welcome to my Dungeons & Dragons Character Sheet Creation tool!");
         }
     }
 }
