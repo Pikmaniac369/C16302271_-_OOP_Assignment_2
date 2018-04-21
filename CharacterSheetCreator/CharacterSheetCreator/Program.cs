a672897b29ba6646f800efff5f5a69386f480e93
Adam Ryan <C16302271@mydit.ie>
2018-04-21 10:58:43 +0100

    Created the basic outline for the classes and races

--- CharacterSheetCreator/CharacterSheetCreator/Program.cs
+++ CharacterSheetCreator/CharacterSheetCreator/Program.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 
 namespace CharacterSheetCreator
 {
+    struct DnDCharacter
+    {
+        private string name;
+        private int age;
+        private DndClass characterClass;
+        
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -22,5 +30,134 @@ namespace CharacterSheetCreator
         {
             Console.WriteLine("Welcome to my Dungeons & Dragons Character Sheet Creation tool!");
         }
+    }
+
+
+    //Abstract classes for DnDClasses and DnDRaces:
+    //-----------------------------------------------
+    abstract class DndClass
+    {
+
+    }
+
+    abstract class DnDRace
+    {
+
+    }
+
+
+
+
+    //DnD CLASSES:
+    //----------------------------------------------
+    class Barbarian : DndClass
+    {
+
+    }
+
+    class Bard : DndClass
+    {
+
+    }
+
+    class Cleric : DndClass
+    {
+
+    }
+
+    class Druid : DndClass
+    {
+
+    }
+
+    class Fighter : DndClass
+    {
+
+    }
+
+    class Monk : DndClass
+    {
+
+    }
+
+    class Paladin : DndClass
+    {
+
+    }
+
+    class Ranger : DndClass
+    {
+
+    }
+
+    class Rogue : DndClass
+    {
+
+    }
+
+    class Sorcerer : DndClass
+    {
+
+    }
+
+    class Warlock : DndClass
+    {
+
+    }
+
+    class Wizard : DndClass
+    {
+
+    }
+
+
+
+
+
+    //DnD RACES:
+    //-------------------------------------------------
+    class Dwarf : DnDRace
+    {
+
+    }
+
+    class Elf : DnDRace
+    {
+
+    }
+
+    class Halfling : DnDRace
+    {
+
+    }
+
+    class Human : DnDRace
+    {
+
+    }
+
+    class Dragonborn : DnDRace
+    {
+
+    }
+
+    class Gnome : DnDRace
+    {
+
+    }
+
+    class Half_Elf : DnDRace
+    {
+
+    }
+
+    class Half_Orc : DnDRace
+    {
+
+    }
+
+    class Tiefling : DnDRace
+    {
+
     }
 }
