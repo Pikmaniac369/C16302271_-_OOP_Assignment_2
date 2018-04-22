9e5ac72feb09d399854d18a3b5ec7a6fd1b9f830
Adam Ryan <C16302271@mydit.ie>
2018-04-22 16:47:59 +0100

    Added ability score fields to the DnDCharacter abstract class and added partial main menu functionality

--- CharacterSheetCreator/CharacterSheetCreator/Program.cs
+++ CharacterSheetCreator/CharacterSheetCreator/Program.cs
@@ -49,7 +49,35 @@ namespace CharacterSheetCreator
 
         private static void displayMenu()
         {
+            int answer;
+
             Console.WriteLine("Welcome to my Dungeons & Dragons Character Sheet Creation tool!");
+
+            do
+            {
+                Console.WriteLine("What would you like to do?\n");
+                Console.WriteLine("\tPress 1: Create a new character.");
+                Console.WriteLine("\tPress 2: View the list of characters that already exist.");
+
+                answer = Convert.ToInt32(Console.ReadLine());
+
+                if(answer == 1)
+                {
+                    Console.WriteLine("\nOkay then! Let's make a new character!");
+                }
+                else if(answer == 2)
+                {
+                    if(DnDCharacter.getNumOfCharacters() == 0)
+                    {
+                        Console.WriteLine("\nThere are currently no characters created!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, but that is not a valid input. Please select either option 1 or option 2 from the main menu!\n");
+                }
+            }
+            while ( (answer != 1) && (answer != 2) );
         }
     }
 
@@ -68,6 +96,14 @@ namespace CharacterSheetCreator
         Gender sex;
         SizeCategory size;
         Race race;
+        private int str;//Strength ability score
+        private int dex;//Dexterity ability score
+        private int con;//Constitution ability score
+        private int inte;//Intelligence ability score
+        private int wis;//Wisdom ability score
+        private int cha;//Charisma ability score
+
+        public static int numOfCharacters = 0;
 
         //DON'T LET THIS BE CALLED IF ANY NUMBER OTHER THAN THE RACE ONES ARE CHOSEN!!!!!!!!!!!!!!!
         public void createCharacter(string namae, int anni, bool morf, int selectedRace)
@@ -126,6 +162,8 @@ namespace CharacterSheetCreator
                     break;
             }
 
+            numOfCharacters++;
+
             /*switch(race)
             {
                 case Race.Dwarf:
@@ -156,68 +194,78 @@ namespace CharacterSheetCreator
                     break;
             }*/
         }
+
+        public static int getNumOfCharacters()
+        {
+            return numOfCharacters;
+        }
     }
 
+
+    public interface IHitPointRoller
+    {
+
+    }
 
 
     //DnD CLASSES:
     //----------------------------------------------
-    class Barbarian : DnDCharacter
+    class Barbarian : DnDCharacter, IHitPointRoller
     {
 
     }
 
-    class Bard : DnDCharacter
+    class Bard : DnDCharacter, IHitPointRoller
     {
 
     }
 
-    class Cleric : DnDCharacter
+    class Cleric : DnDCharacter, IHitPointRoller
     {
 
     }
 
-    class Druid : DnDCharacter
+    class Druid : DnDCharacter, IHitPointRoller
     {
 
     }
 
-    class Fighter : DnDCharacter
+    class Fighter : DnDCharacter, IHitPointRoller
     {
 
     }
 
-    class Monk : DnDCharacter
+    class Monk : DnDCharacter, IHitPointRoller
     {
 
     }
 
-    class Paladin : DnDCharacter
+    class Paladin : DnDCharacter, IHitPointRoller
     {
         
     }
 
-    class Ranger : DnDCharacter
+    class Ranger : DnDCharacter, IHitPointRoller
     {
 
     }
 
-    class Rogue : DnDCharacter
+    class Rogue : DnDCharacter, IHitPointRoller
     {
 
     }
 
-    class Sorcerer : DnDCharacter
+    class Sorcerer : DnDCharacter, IHitPointRoller
     {
 
     }
 
-    class Warlock : DnDCharacter
+    class Warlock : DnDCharacter, IHitPointRoller
     {
 
     }
 
-    class Wizard : DnDCharacter
+    class Wizard : DnDCharacter, IHitPointRoller
     {
 
     }
